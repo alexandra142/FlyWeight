4a3cbed104b6f4bcf797b89c7bdc2073cbde3721
Alexandra Pistrakova <alex.pistrakova@gmail.com>
2019-01-16 00:01:26 +0100

    DiamondRepository Created

---
+++ Diamonds/Diamonds/DiamondRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Diamonds
+{
+    internal class DiamondRepository
+    {
+        public int DiamondKinds = Enum.GetNames(typeof(DiamondType)).Length;
+        public Random Random = new Random();
+        public IDiamond[] diamonds;
+
+        public DiamondRepository()
+        {
+            diamonds = new IDiamond[DiamondKinds];
+
+        }
+
+        /// <summary>
+        /// Get Next index of not none diamond
+        /// </summary>
+        /// <returns></returns>
+        public int NextIndexOfDiamond()
+        {
+            return Random.Next(1, DiamondKinds);
+        }
+
+        public DiamondType Next()
+        {
+            return (DiamondType)NextIndexOfDiamond();
+        }
+    }
+}
